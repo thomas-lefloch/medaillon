0960cc01859f4d98e4804269b1dff34f9323cc13
thomas-lefloch <t.lefloch53@gmail.com>
2025-10-21 10:09:27 +0200

    filtering à la main

--- quality/gx_checkoint.py
+++ quality/gx_checkoint.py
@@ -1,20 +1,30 @@
 # Question:
-# - avant ou après la transformation en parquet ??
-# - Pourquoi gx_checkpoint ??
+# - avant ou après la transformation en parquet ?? apres (début silver)
+# - Pourquoi gx_checkpoint ?? Great Expectation (biblio)
 # - Que fait le script si il detecte des erreurs / données non valables ?
-
+# suppression
 
 # - Quelles programmes pour neo4j_bulk_import ?
+# requete cypher en python dans .sh
 
-def all_nodes_have_unique_ids(nodes):
-    ids = set()
+
+# return the list of uniques nodes and a set of all unique ids
+def filter_nodes_without_unique_id(nodes: list[dict]) -> list[dict]:
+    uniques_ids = set()
+    valid_nodes = []
     for node in nodes:
-        if node["id"] in ids:
-            print(f"Check failed! duplicated id found: ", node)
-            exit(1)
-        ids.add(node["id"])
+        if node["id"] in uniques_ids:
+            continue
+        valid_nodes.append(node)
+        uniques_ids.add(node["id"])
 
-def edges_are_not_missing_ids(edges):
+    return valid_nodes
+
+
+# return the list of edges that don't have None for src and dest
+def filter_edges_with_missing_ids(edges: list[dict]) -> list[dict]:
+    valid_edges = []
     for edge in edges:
-        if edge["src"] == None or edge["dest"] == None:
-            print("Check failed ! Edge with missing id found: ", edge)
+        if edge["src"] != None and edge["dest"] != None:
+            valid_edges.append(edge)
+    return valid_edges
