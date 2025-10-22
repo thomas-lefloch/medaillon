8a052a477199493106e16c9e4a6627062c68de09
thomas-lefloch <t.lefloch53@gmail.com>
2025-10-22 13:57:28 +0200

    insertion des données dans neo4J

--- scripts/neo4j_bulk_import.sh
+++ scripts/neo4j_bulk_import.sh
@@ -1,11 +1,20 @@
 python <<EOF
 import pandas as pd
+import os
 
 nodes = pd.read_parquet("data/silver/nodes.parquet")
-nodes = nodes.rename({"id": "id:ID"})
+nodes = nodes.rename({"id": "id:ID", "label": ":LABEL"}, axis="columns")
 nodes.to_csv("data/gold/nodes.csv", index=False)
 
 edges = pd.read_parquet("data/silver/edges/")
-edges = edges.rename({"src": ":START_ID", "dest": ":END_ID"})
+edges = edges.rename({"src": ":START_ID", "dest": ":END_ID", "type": ":TYPE"}, axis="columns").drop("shard", axis="columns")
 edges.to_csv("data/gold/edges.csv", index=False)
 EOF
+
+docker compose --remove-orphans run\
+    neo4j bin/neo4j-admin database import full\
+    --id-type integer\
+    --nodes //import/nodes.csv\
+    --relationships //import/edges.csv\
+    --overwrite-destination\
+    
