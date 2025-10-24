531945a6125d0c877b87cbfa88bc620eb81eb419
Thomas Le Floch <t.lefloch53@gmail.com>
2025-10-24 15:34:39 +0200

    routes /entity/{id} et /query/cypher

--- api/main.py
+++ api/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
-import neo4j
+from neo4j import GraphDatabase
+import os
 
 app = FastAPI()
+neo4j_uri = os.environ["NEO4J_URI"]
 
 
 @app.get("/health")
 def health() -> dict[str, str]:
-    return {"status": "healthy" }
+    return {"status": "healthy"}
+
 
 @app.get("/entity/{id}")
 def get_entity_by_id(id: int):
-    pass
+    with GraphDatabase.driver(neo4j_uri) as driver:
+        res = driver.execute_query("MATCH (n:Node{id:$id}) RETURN n", {"id": id})
+        return res.records
+
 
 @app.post("/query/cypher")
-def make_cypher_request():
-    pass
+def make_cypher_request(query: str):
+    with GraphDatabase.driver(neo4j_uri) as driver:
+        res = driver.execute_query(query)
+        return res.records
